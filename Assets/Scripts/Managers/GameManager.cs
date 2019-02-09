d7e2b343416a8dce305e4948258915f27101b6d4
m3talpillow <jaredjohans@gmail.com>
2019-02-09 17:06:49 -0500

    Combat scene began

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private ActorSuper[] party;
+
+    void Awake()
+    {
+        party = new ActorSuper[1];
+        party[0] = new Healer();
+    }
+}
