e6bfa5767596bb0b8f38975a992f06fa5bae713f
Joseph Peaden <joe.peaden@gmail.com>
2019-02-09 19:09:01 -0500

    Updated Level Prefabs

---
+++ Assets/Scripts/EncounterRoom.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncounterRoom : Room
+{
+    override public void SetExplored(bool isExplored)
+    {
+        GenerateEncounter();
+        base.SetExplored(isExplored);
+    }
+
+    private void GenerateEncounter()
+    {
+        Debug.Log("An encounter takes place!");
+    }
+}
--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -16,7 +16,7 @@ public class Level : MonoBehaviour
         for (int i = 0; i < transform.childCount; i++)
         {
             Room room = transform.GetChild(i).gameObject.GetComponent<Room>();
-            
+
             // be sure to tag the first room
             if (room.gameObject.tag == "StartRoom")
             {
@@ -24,6 +24,8 @@ public class Level : MonoBehaviour
                 currentRoom = room;
                 room.SetExplored(true);
             }
+            else
+                room.SetExplored(false);
 
             rooms.Add(room);
         }
@@ -33,16 +35,17 @@ public class Level : MonoBehaviour
 
     public void MoveParty(Room room)
     {
-        // if party is not in adjacent room and not explored, do not move
-        if (!currentRoom.adjacentRooms.Contains(room) && !room.IsExplored())
-            return;
 
-        currentRoom = room;
+        if (room.IsVisible())
+        {
+            currentRoom = room;
 
-        // move player party to center of next room
-        playerParty.transform.position = currentRoom.transform.position;
+            // move player party to center of next room
+            playerParty.transform.position = currentRoom.transform.position;
 
-        room.SetExplored(true);
+            if(!room.IsExplored())
+                room.SetExplored(true);
+        }
     }
 
 }
